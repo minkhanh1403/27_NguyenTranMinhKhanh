06acabf40e9315ebcf9e264a059de235d8b7682f
LiemMinhHuy <liemminhhuy2003@gmail.com>
2024-03-08 14:44:31 +0700

    Huy gui file

---
+++ 26_LiemMinhHuy_Helloworld.cpp
@@ -0,0 +1,11 @@
+// helloworld.cpp : Defines the entry point for the console application.
+//
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello HowKTeam.com!" << endl;
+	cout << end;
+	return 0;
+}
